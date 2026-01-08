0f6893a3203e44c56386cc64d859e606c9bd6afa
user123 <144436875+Gerberg1@users.noreply.github.com>
2026-01-08 12:40:05 +0100

    ready for github pages

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 // config.js
 // Konfiguration + mock-data + URL-parametre
 export function API_BASE_URL() {
-    return "http://localhost:8080/api/ticketservice"
-    //return "https://ticketservicemas-b4addvfjf6hthedj.norwayeast-01.azurewebsites.net/api/ticketservice"
+    //return "http://localhost:8080/api/ticketservice"
+    return "https://ticketservicemas-b4addvfjf6hthedj.norwayeast-01.azurewebsites.net/api/ticketservice"
 }
 
 export function AUTH_BASE_URL() {
-    return "http://localhost:8080"
-    //return "https://ticketservicemas-b4addvfjf6hthedj.norwayeast-01.azurewebsites.net"
+    //return "http://localhost:8080"
+    return "https://ticketservicemas-b4addvfjf6hthedj.norwayeast-01.azurewebsites.net"
 }
 export const USE_MOCK = false;
 
